3c22b1893b319318cc864bcc97a04fcbe9c65cd0
Straw81 <jpms.dinossauro@gmail.com>
2023-05-27 23:59:33 -0300

    numeros letras e simbolos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 (document).ready(function () {
-    
+    var abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var numbers = '0123456789'.split('');
+    var symbols = '!@#$%^&*()_+-=[]{}:;,./<>?'.split('');
 })
