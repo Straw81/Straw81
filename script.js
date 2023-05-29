989ed2ed58ef8826b9fbd10768eb907b6784702c
Straw81 <jpms.dinossauro@gmail.com>
2023-05-29 16:47:37 -0300

    incluindo novas páginas

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-(document).ready(function () {
-var abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var numbers = '0123456789'.split('');
-var symbols = '!@#$%^&*()_+-=[]{}:;,./<>?'.split('');
-    $('#variaveis').click(function () {
-            <summary>HTML</summary>
-        window.location.href = "https://github.com/Straw81/Pj2/blob/teste/js/variaveis/var.html"
-    })
+$(document).ready(function () {
+    var abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var numbers = '0123456789'.split('');
+    var symbols = '!@#$%^&*()_+-=[]{}:;,./<>?'.split('');
+
+
 })
