374b0105eaef29868aefac1b10ff9f02230dab08
Straw81 <jpms.dinossauro@gmail.com>
2023-05-28 21:57:40 -0300

    trocando de pagina

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 (document).ready(function () {
-    var abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    var numbers = '0123456789'.split('');
-    var symbols = '!@#$%^&*()_+-=[]{}:;,./<>?'.split('');
-    
+var abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var numbers = '0123456789'.split('');
+var symbols = '!@#$%^&*()_+-=[]{}:;,./<>?'.split('');
+    $('#variaveis').click(function () {
+            <summary>HTML</summary>
+        window.location.href = "https://github.com/Straw81/Pj2/blob/teste/js/variaveis/var.html"
+    })
 })
